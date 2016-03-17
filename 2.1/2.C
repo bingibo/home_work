61290606534f835913f29c68cde6d6e8fcbf50c2
bingibo <altertema2@yandex.ru>
2016-03-17 23:49:03 +0300

    abc

---
+++ 2.1/2.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+  int main(void)
+{
+  int day = 6, month = 1;
+  printf("%d/%d", day,month );
+  return 0;
+}
+
---
+++ 2.1/3.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(void) 
+{ 	
+    int day = 6,month = 1,change;
+    change=day;
+    day=month;
+    month=change;
+
+    printf("%d/%d",day,month );
+    return 0;
+}
+
+	
+
+
+	
